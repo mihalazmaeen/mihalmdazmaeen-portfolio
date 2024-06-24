f474ebf76d0574658e9a9bbec1a1da3d518771dd
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-06-24 12:11:09 +0600

    server change

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const app = express();
 const dbConfig = require("./config/dbConfig");
 const portfolioRoute = require("./routes/portfolioRoute");
+
 app.use(express.json());
-const path = require("path");
-if(process.env.NODE_ENV !== "production"){
+app.use("/api/portfolio", portfolioRoute);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
-app.use("/api/portfolio", portfolioRoute);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const port = process.env.PORT || 5000;
 
